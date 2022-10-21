29d3d4a1958e2648cd7908e7ad02db243283d1c0
Adrien GARCIA <adrien@casual-studio.com>
2022-10-21 11:07:51 +0200

    depot fichiers

--- Desktop/Nouveau dossier (2)/HttpTrigger1/__init__.py
+++ Desktop/Nouveau dossier (2)/HttpTrigger1/__init__.py
@@ -22,3 +22,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+storage_account_key = "YF1+wBjHT1g7JnwUyaiOBGrsNSnh2ydrpUrFEBo/vx5NxU+RiLuyMpP+lLTa3Qld1lYh+QULBEAe+AStur4KMQ=="
+nom_compte_stockage = "storageaccountadrien"
+connection_string = "DefaultEndpointsProtocol=https;AccountName=storageaccountadrien;AccountKey=gVKIJjA0dCYn5QFZBHk/l5dtTdu6imKpoZPScvqbuuI8TBRiy4hXeQliIESrpSsAEYrx2LNfQdq++ASt3CdpiA==;EndpointSuffix=core.windows.net"
+nom_conteneur = "file-upload"
+
+def uploadToBlobStorage(file_path,file_name): 
+   blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+   blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
+   open(file_path,"rb") 
+   blob_client.upload_blob(données)
+   print(f"Téléchargé {nom_fichier}.")
+
+uploadToBlobStorage('PATH_OF_FILE_TO_UPLOAD','FILE_NAME')
+
